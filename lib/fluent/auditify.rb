13388d59f1f2331f3a7fdbb3addad068259080d5
Kentaro Hayashi <hayashi@clear-code.com>
2025-07-12 13:06:20 +0900

    Import template files
    $ bundle gem fluent-auditify

---
+++ lib/fluent/auditify.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "auditify/version"
+
+module Fluent
+  module Auditify
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/fluent/auditify/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Fluent
+  module Auditify
+    VERSION = "0.1.0"
+  end
+end
