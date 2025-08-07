4a5fbb83a292edb53d1fc2692608cb3de4f77e99
Kentaro Hayashi <hayashi@clear-code.com>
2025-08-07 22:20:48 +0900

    Add missing plugin class
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

---
+++ lib/fluent/auditify/plugin.rb
@@ -0,0 +1,42 @@
+require 'fluent/config/error'
+require 'fluent/auditify/registry'
+require 'logger'
+
+module Fluent
+  module Auditify
+    module Plugin
+
+      # e.g. PATH_TO_...lib/fluent/auditify/plugin
+      DEFAULT_PLUGIN_PATH = File.expand_path('../plugin', __FILE__)
+      # e.g. PATH_TO_...lib/fluent/auditify
+      FLUENT_AUDITIFY_LIB_PATH = File.dirname(DEFAULT_PLUGIN_PATH)
+
+      CONF_REGISTRY = Registry.new(:conf, 'fluent/auditify/conf_')
+
+      REGISTRIES = [CONF_REGISTRY]
+
+      CHARGES = []
+
+      def self.charges
+        CHARGES
+      end
+
+      def self.registries
+        REGISTRIES
+      end
+
+      # This method will be executed when require it
+      def self.register_conf(plugin_name, plugin_klass)
+        if !plugin_klass.is_a?(Class) and
+          !([:supported_platform?, :parse].all? { |v| plugin_klass.respond_to?(v) })
+          raise Fluent::ConfigError, "Invalid Fluent Auditify plugin implementation as 'conf' plugin: <#{plugin_name}>."
+        end
+        CONF_REGISTRY.register(:conf, plugin_name, plugin_klass)
+      end
+
+      def self.guilty(message, options={})
+        CHARGES.push([message, options])
+      end
+    end
+  end
+end
