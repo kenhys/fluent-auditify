0b2b16db5b01f2a089959a687d5a531cc1b13301
Kentaro Hayashi <hayashi@clear-code.com>
2025-08-16 22:12:11 +0900

    Add directive test case and implementation
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- lib/fluent/auditify/plugin/conf_yaml.rb
+++ lib/fluent/auditify/plugin/conf_yaml.rb
@@ -2,6 +2,7 @@ require 'fluent/config/error'
 require 'fluent/auditify/plugin/conf'
 require 'fluent/config/yaml_parser'
 require 'yaml'
+require 'term/ansicolor'
 
 module Fluent::Auditify::Plugin
   class YamlConf < Conf
@@ -16,6 +17,7 @@ module Fluent::Auditify::Plugin
     end
 
     def initialize
+      super
     end
 
     def parse(conf)
@@ -37,6 +39,109 @@ module Fluent::Auditify::Plugin
           end
         end
       end
+    end
+
+    def standard_detector?(method)
+      method.to_s.start_with?('detect_') and
+        not method.to_s.end_with?('_fallback')
+    end
+
+    def fallback_detector?(method)
+      method.to_s.start_with?('detect_') and
+        method.to_s.end_with?('_fallback')
+    end
+
+    def unknown_directive?(root)
+      root.elements.empty?
+    end
+
+    #
+    # Standard syntax error detector
+    # In this case, YamlParser doesn't raise exception, so
+    # check generated root elements.
+    #
+
+    #
+    # config:
+    #   - unknown:
+    #       $type: stdout
+    #
+    def detect_wrong_directive(root, conf)
+      return if [] != root.elements
+
+      # no valid root elements here
+      yaml = YAML.load(file_get_contents(conf))
+      unknowns = yaml.keys.select { |v| not %w(system: config:).include?(v) }
+      file_readlines_each(conf) do |line, index|
+        unknowns.each do |directive|
+          if line.chomp.include?(directive)
+            guilty("top level directive must be system or config, not <#{directive}>",
+                   { path: conf, line: index + 1, content: line.chomp})
+          end
+        end
+      end
+    end
+
+    #
+    # config:
+    #   - match:
+    #       $type: stdout
+    #
+    def detect_no_source(root, conf)
+      return if unknown_directive?(root)
+      unless root.elements.any? { |v| 'source' == v.name }
+        guilty("no source directive", { path: conf })
+      end
+    end
+
+    #
+    # config:
+    #   - source:
+    #       $type: stdout
+    #
+    def detect_no_match(root, conf)
+      return if unknown_directive?(root)
+      unless root.elements.any? { |v| 'match' == v.name }
+        guilty("no match directive", { path: conf })
+      end
+    end
+
+    #
+    # Fallback syntax error detector
+    # In this case, YamlParser raise exception, so
+    # parse it as YAML again then check generated YAML contents.
+    #
+
+    #
+    # config:
+    #   - source:
+    #     $type: stdout
+    #
+    def detect_broken_config_param_indent_fallback(yaml, conf)
+      # source unless ((yaml.keys == ["config"]) or (yaml.keys == ["system", "config"]))
+      return unless yaml['config']
+
+      nth = 0
+      yaml['config'].each do |element|
+        if element.has_key?('source')
+          nth += 1
+        end
+        
+        # source: and the following parameter must have intent
+        count = 0
+        file_readlines_each(conf) do |line, i|
+          if line.include?('source:')
+            count += 1
+            if nth == count
+              guilty("source must not be empty. parameter for <source:> indent might be broken",
+                     { path: conf, line: i + 1, content: line})
+            end
+          end
+        end
+      end
+    end
+
+    def detect_invalid_config_elements(root, conf)
     end
   end
 end
