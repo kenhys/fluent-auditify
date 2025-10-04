d55305a16f96c413ad79664ca23367300dec4609
Kentaro Hayashi <hayashi@clear-code.com>
2025-10-04 22:28:34 +0900

    Add test helper

---
+++ lib/fluent/auditify/helper/test.rb
@@ -0,0 +1,35 @@
+module Fluent
+  module Auditify
+    module Helper
+      module Test
+        def test_fixture_path(path)
+          File.join(File.expand_path('../fixtures', __FILE__), path)
+        end
+
+        def discard
+          Fluent::Auditify::Plugin.discard
+        end
+
+        def test_mask_charges(options={})
+          masked = []
+          Fluent::Auditify::Plugin.charges.each do |entry|
+            attrs = entry.last
+            value = {
+              path: File.basename(attrs[:path]),
+              content: attrs[:content],
+              line: attrs[:line]
+            }
+            if options[:omit_line]
+              value.delete(:line)
+            end
+            if options[:strip_content]
+              value[:content].strip!
+            end
+            masked << [entry.first, value]
+          end
+          masked
+        end
+      end
+    end
+  end
+end
